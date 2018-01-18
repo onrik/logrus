f2ebfbcfcd5240acb4aeb7321e31b123d460eb31
Andrey <and@rey.im>
2018-01-18 11:56:59 +0300

    Added function name logging.

--- filename/filename.go
+++ filename/filename.go
@@ -9,9 +9,10 @@ import (
 )
 
 type Hook struct {
-	Field  string
-	Skip   int
-	levels []logrus.Level
+	Field     string
+	Skip      int
+	levels    []logrus.Level
+	Formatter func(file, function string, line int) string
 }
 
 func (hook *Hook) Levels() []logrus.Level {
@@ -19,7 +20,7 @@ func (hook *Hook) Levels() []logrus.Level {
 }
 
 func (hook *Hook) Fire(entry *logrus.Entry) error {
-	entry.Data[hook.Field] = findCaller(hook.Skip)
+	entry.Data[hook.Field] = hook.Formatter(findCaller(hook.Skip))
 	return nil
 }
 
@@ -28,6 +29,9 @@ func NewHook(levels ...logrus.Level) *Hook {
 		Field:  "source",
 		Skip:   5,
 		levels: levels,
+		Formatter: func(file, function string, line int) string {
+			return fmt.Sprintf("%s:%d", file, line)
+		},
 	}
 	if len(hook.levels) == 0 {
 		hook.levels = logrus.AllLevels
@@ -36,22 +40,32 @@ func NewHook(levels ...logrus.Level) *Hook {
 	return &hook
 }
 
-func findCaller(skip int) string {
-	file := ""
-	line := 0
+func findCaller(skip int) (string, string, int) {
+	var (
+		pc       uintptr
+		file     string
+		function string
+		line     int
+	)
 	for i := 0; i < 10; i++ {
-		file, line = getCaller(skip + i)
+		pc, file, line = getCaller(skip + i)
 		if !strings.HasPrefix(file, "logrus") {
 			break
 		}
 	}
-	return fmt.Sprintf("%s:%d", file, line)
+	if pc != 0 {
+		frames := runtime.CallersFrames([]uintptr{pc})
+		frame, _ := frames.Next()
+		function = frame.Function
+	}
+
+	return file, function, line
 }
 
-func getCaller(skip int) (string, int) {
-	_, file, line, ok := runtime.Caller(skip)
+func getCaller(skip int) (uintptr, string, int) {
+	pc, file, line, ok := runtime.Caller(skip)
 	if !ok {
-		return "", 0
+		return 0, "", 0
 	}
 
 	n := 0
@@ -65,5 +79,5 @@ func getCaller(skip int) (string, int) {
 		}
 	}
 
-	return file, line
+	return pc, file, line
 }
