2ae0a60c780bbe800c83855a76c167e2ebfd03a5
Andrey <and@rey.im>
2020-01-23 14:13:25 +0300

    Add Depth to filename.

---
+++ filename/filename_test.go
@@ -0,0 +1,27 @@
+package filename
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+)
+
+func TestFilenameHook(t *testing.T) {
+	hook := NewHook()
+
+	buff := new(bytes.Buffer)
+	logrus.SetOutput(buff)
+	logrus.AddHook(hook)
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	logrus.Info("Test")
+
+	require.NotEqual(t, "", buff.String())
+	require.Equal(t,
+		"filename/filename_test.go:20",
+		gjson.Get(buff.String(), DefaultField).Str,
+	)
+}
